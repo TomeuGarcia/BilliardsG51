c444f5cf2a967f1bbe71f3461c67fa310f3ad3c0
TomeuGarcia <tomeu.garcia.p@gmail.com>
2025-03-22 11:45:23 +0100

    Setup SDL

---
+++ source/BilliardsSDL/Application/BilliardsApplication.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdio>
+
+class BilliardsApplication
+{
+public:
+	BilliardsApplication();
+	~BilliardsApplication();
+	void Run();
+
+private:
+	void InitSDL();
+};
+
---
+++ source/BilliardsSDL/main.cpp
@@ -0,0 +1,7 @@
+#include "Application/BilliardsApplication.h"
+
+int main()
+{
+	BilliardsApplication application{};
+	application.Run();
+}
