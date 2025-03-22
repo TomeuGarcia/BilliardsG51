dd4e8161465bad22010637ac44c4f85ea003807d
TomeuGarcia <tomeu.garcia.p@gmail.com>
2025-03-22 11:45:23 +0100

    Setup SDL

---
+++ source/BilliardsSDL/Application/BilliardsApplication.cpp
@@ -0,0 +1,19 @@
+#include "BilliardsApplication.h"
+
+BilliardsApplication::BilliardsApplication()
+{
+	printf("App created\n");
+}
+
+BilliardsApplication::~BilliardsApplication()
+{
+	printf("App destroyed\n");
+}
+
+void BilliardsApplication::Run()
+{
+}
+
+void BilliardsApplication::InitSDL()
+{
+}
